4bd37e4901d286b7ced2e3a6b2d3e0df743010eb
comconToukir <ahmdtoukir@gmail.com>
2022-10-10 23:22:47 +0600

    google-font added motserrat & roboto condensed, header component added with active navlinks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,22 @@
+import { NavLink } from 'react-router-dom';
+
+import logo from '../../logo.webp'
+
 import './Header.scss';
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <nav>
+      <div className="container">
+        <img src={logo} alt="" />
+        <span>QUIZ CONTEST</span>
+        <div className='nav-links'>
+          <NavLink className={({isActive}) => isActive ? 'active': null} to='/' end>Home</NavLink>
+          <NavLink className={({isActive}) => isActive ? 'active': null} to='/stats'>Stats</NavLink>
+          <NavLink className={({isActive}) => isActive ? 'active': null} to='/blog'>Blog</NavLink>
+        </div>
+      </div>
+    </nav>
   );
 };
 
