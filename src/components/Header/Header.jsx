2701a7dd67fc35dbd505cca3e0c8e186db3715df
comconToukir <ahmdtoukir@gmail.com>
2022-10-11 22:14:31 +0600

    readme file added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,21 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 import logo from "../../logo.webp";
 
 import "./Header.scss";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => navigate("/");
+  
   return (
     <nav>
       <div className="container">
-        <img src={logo} alt="" />
-        <span>QUIZ CONTEST</span>
+        <div onClick={handleNavigate} className='nav-logo'>
+          <img src={logo} alt="" />
+          <span>QUIZ CONTEST</span>
+        </div>
         <div className="nav-links">
           <NavLink
             className={({ isActive }) => (isActive ? "active" : null)}
